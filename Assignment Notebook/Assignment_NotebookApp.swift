bbcb5240b18e271db7e994a3935206fee50726fb
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-01-09 09:29:15 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Chris Markiewicz on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
